9051e03b3023060cb55359e8db6a231452322e33
HRM King David II <uscapitalfundingii@gmail.com>
2026-04-06 10:49:02 +0400

    Create account_balances_lang.php

---
+++ crm/modules/account_balances/language/turkish/account_balances_lang.php
@@ -0,0 +1,77 @@
+<?php
+
+// Version 105
+$lang['account_balance_enable_multi_currency_for_invoice'] = 'Çoklu para seçimini faturada aktifleştir';
+$lang['account_balance_enable_multi_currency_for_estimate'] = 'Çoklu para seçimini proformada aktifleştir';
+
+
+
+
+// Version 102
+
+$lang['account_week_balance_title']     = 'Haftalık Bakiye Grafiği';
+$lang['account_entry']                  = 'Gelirler';
+$lang['account_income_new']             = 'Yeni Gelir';
+$lang['income_success_msg']             = 'Gelir kaydı başarıyla işlendi!';
+$lang['income_not_found']               = 'Gelir kaydı bulunamadı!';
+
+
+$lang['account_balance_money_in']       = 'Para Giriş';
+$lang['account_balance_money_out']      = 'Para Çıkış';
+$lang['account_balance_carryover']      = 'Devir Bakiyesi';
+
+
+// Version. 101
+$lang['account_withdrawal']             = 'Kasa Para Çıkışı';
+$lang['account_withdrawal_new']         = 'Yeni Para Çıkışı';
+$lang['money_withdraw']                 = 'Para Çıkışı';
+
+
+
+
+//kasa yönetimi
+$lang['case_account']                   = 'Kasa / Bakiye';
+$lang['case_manage']                    = 'Kasa / Banka Yönetimi';
+$lang['case_list']                      = 'Kasa / Banka Listesi';
+$lang['case']                           = 'Kasa';
+$lang['case_history']                   = 'Kasa Hareketleri';
+$lang['balance']                        = 'Bakiye';
+$lang['balance_due']                    = 'Bakiye';
+$lang['statement_heading_date']         = 'Tarih';
+$lang['statement_heading_details']      = 'İşlem Açıklama';
+$lang['statement_heading_amount']       = 'Tutar';
+$lang['statement_heading_payments']     = 'Tahsilat';
+$lang['statement_heading_balance']      = 'Bakiye';
+$lang['statement_invoice_details']      = 'Fatura %s - Vade Tarihi %s';
+$lang['statement_payment_details']      = '%s nolu Tahsilat, Fatura %s';
+
+//transfer
+$lang['account_balances_transfer']      = 'Transfer';
+$lang['inter_cash_transfer']            = 'Kasalar Arası Transfer';
+$lang['new_transfer_record']            = 'Yeni Transfer Kaydı';
+$lang['transfer_heading_id']            = 'ID';
+$lang['transfer_heading_welding_case']  = 'Kaynak Kasa';
+$lang['transfer_heading_target_case']   = 'Hedef Kasa';
+$lang['transfer_heading_source_amount'] = 'Gönderilen Tutar';
+$lang['transfer_heading_target_amount'] = 'Alınan Tutar';
+$lang['transfer_heading_description']   = 'Açıklama';
+$lang['transfer_heading_date']          = 'Transfer Tarihi';
+$lang['transfer_heading_process_date']  = 'İşlem Tarihi';
+$lang['transfer_heading_process_staff'] = 'İşlem Yapan';
+$lang['transfer_warning_msg']           = 'Farklı kasalar arasında transfer yapmayı deneyebilirsiniz :)';
+$lang['transfer_success_msg']           = 'İşlem başarılı şekilde yapıldı';
+$lang['transfer_not_found']             = 'Transfer kaydı bulunamadı';
+$lang['transfer_edit_heading']          = 'Transfer Kaydı Düzenle';
+$lang['transfer_add_heading']           = 'Transfer Kaydı Ekle';
+
+//hesap devir bakiyesi
+$lang['account_balances_deliver']       = 'Hesap Devir Bakiyesi';
+$lang['balance_heading_account']        = 'Kasa';
+$lang['account_balances_id']            = 'ID';
+$lang['balance_heading_open_balance']   = 'Açılış Bakiyesi';
+$lang['balance_heading_currency']       = 'Para Birimi';
+$lang['balance_not_found']              = 'Kasa Bulunamadı!';
+$lang['balance_success_msg']            = 'Hesap devir bakiyesi başarılı bir şekilde güncellendi';
+$lang['cash_opening_deliver_balance']   = 'Kasa Açılış / Devir Bakiyesi';
+$lang['transfer_of_money_from_cash']    = 'kasasından para transferi.';
+$lang['money_transfer_to_cash']         = 'kasasına para transferi.';
