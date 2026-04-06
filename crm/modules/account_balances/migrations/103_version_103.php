d6f983b313338ea144722af65c1eb75a9a01a802
HRM King David II <uscapitalfundingii@gmail.com>
2026-04-06 10:49:12 +0400

    Create 103_version_103.php

---
+++ crm/modules/account_balances/migrations/103_version_103.php
@@ -0,0 +1,17 @@
+<?php
+
+defined('BASEPATH') || exit('No direct script access allowed');
+
+class Migration_Version_103 extends App_module_migration
+{
+
+    public function up()
+    {
+
+        $CI = &get_instance();
+
+
+
+    }
+
+}
