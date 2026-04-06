121f8d8e8635b03591912427c36b8708d52b4680
HRM King David II <uscapitalfundingii@gmail.com>
2026-04-06 10:48:31 +0400

    Create account_balances_lang.php

---
+++ crm/modules/account_balances/language/english/account_balances_lang.php
@@ -0,0 +1,74 @@
+<?php
+
+// Version 105
+$lang['account_balance_enable_multi_currency_for_invoice'] = 'Enable multi-currency on Invoices';
+$lang['account_balance_enable_multi_currency_for_estimate'] = 'Enable multi-currency on Estimates';
+
+
+
+// Version 102
+
+$lang['account_week_balance_title']     = 'Weekly Balance Chart';
+$lang['account_entry']                  = 'Account Income';
+$lang['account_income_new']             = 'New Income';
+$lang['income_success_msg']             = 'Income record processed successfully!';
+$lang['income_not_found']               = 'Income record Not Found!';
+
+
+$lang['account_balance_money_in']       = 'Money In';
+$lang['account_balance_money_out']      = 'Money Out';
+$lang['account_balance_carryover']      = 'Carryover Balance';
+
+// Version. 101
+$lang['account_withdrawal']             = 'Account Withdrawal';
+$lang['account_withdrawal_new']         = 'New Withdrawal';
+$lang['money_withdraw']                 = 'Withdraw';
+
+
+
+// Cash Management
+$lang['case_account']                   = 'Cash/Bank Balance';
+$lang['case_manage']                    = 'Cash/Bank Manage';
+$lang['case_list']                      = 'Cash/Bank List';
+$lang['case']                           = 'Cash/Bank';
+$lang['case_history']                   = 'Cash/Bank Transaction';
+$lang['balance']                        = 'Balance';
+$lang['balance_due']                    = 'Balance';
+$lang['statement_heading_date']         = 'Date';
+$lang['statement_heading_details']      = 'Transaction Details';
+$lang['statement_heading_amount']       = 'Amount';
+$lang['statement_heading_payments']     = 'Payments';
+$lang['statement_heading_balance']      = 'Balance';
+$lang['statement_invoice_details']      = 'Invoice %s - Due Date %s';
+$lang['statement_payment_details']      = 'Payment %s, Invoice %s';
+
+// Transfer
+$lang['account_balances_transfer']      = 'Transfer';
+$lang['inter_cash_transfer']            = 'Cash/Bank Transfer';
+$lang['new_transfer_record']            = 'New Transfer Record';
+$lang['transfer_heading_id']            = 'ID';
+$lang['transfer_heading_welding_case']  = 'Source Cash/Bank';
+$lang['transfer_heading_target_case']   = 'Target Cash/Bank';
+$lang['transfer_heading_source_amount'] = 'Sent Amount';
+$lang['transfer_heading_target_amount'] = 'Received Amount';
+$lang['transfer_heading_description']   = 'Description';
+$lang['transfer_heading_date']          = 'Transfer Date';
+$lang['transfer_heading_process_date']  = 'Process Date';
+$lang['transfer_heading_process_staff'] = 'Processed by';
+$lang['transfer_warning_msg']           = 'You can try transferring between different Cash/Bank :)';
+$lang['transfer_success_msg']           = 'Transfer was successfully processed';
+$lang['transfer_not_found']             = 'Transfer record not found';
+$lang['transfer_edit_heading']          = 'Edit Transfer Record';
+$lang['transfer_add_heading']           = 'Add Transfer Record';
+
+// Account Opening Balance
+$lang['account_balances_deliver']       = 'Account Opening Balance';
+$lang['balance_heading_account']        = 'Cash/Bank';
+$lang['account_balances_id']            = 'ID';
+$lang['balance_heading_open_balance']   = 'Opening Balance';
+$lang['balance_heading_currency']       = 'Currency';
+$lang['balance_not_found']              = 'Cash/Bank Not Found!';
+$lang['balance_success_msg']            = 'Account opening balance updated successfully';
+$lang['cash_opening_deliver_balance']   = 'Cash Opening / Deliver Balance';
+$lang['transfer_of_money_from_cash']    = 'Transfer money in';
+$lang['money_transfer_to_cash']         = 'Transfer money out';
