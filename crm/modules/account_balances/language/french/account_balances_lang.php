fc4768873a33181d7d63e86760b80385d3575028
HRM King David II <uscapitalfundingii@gmail.com>
2026-04-06 10:48:38 +0400

    Create account_balances_lang.php

---
+++ crm/modules/account_balances/language/french/account_balances_lang.php
@@ -0,0 +1,74 @@
+<?php
+
+// Version 105
+$lang['account_balance_enable_multi_currency_for_invoice'] = 'Activer le multidevise sur les factures';
+$lang['account_balance_enable_multi_currency_for_estimate'] = 'Activer le multidevise sur les estimations';
+
+
+
+// Version 102
+
+$lang['account_week_balance_title']     = 'Tableau du solde hebdomadaire';
+$lang['account_entry']                  = 'Revenu du compte';
+$lang['account_income_new']             = 'Nouveau revenu';
+$lang['income_success_msg']             = 'Enregistrement de revenus traité avec succès !';
+$lang['income_not_found']               = 'Enregistrement de revenu introuvable!';
+$lang['transfer_of_money_from_cash']    = 'Transfer money in';
+
+
+$lang['account_balance_money_in']       = 'Argent entrant';
+$lang['account_balance_money_out']      = 'Plus d\'argent';
+$lang['account_balance_carryover']      = 'Solde de report';
+
+
+// Version. 101
+$lang['account_withdrawal']             = 'Retrait de compte';
+$lang['account_withdrawal_new']         = 'Nouveau retrait';
+$lang['money_withdraw']                 = 'Retirer';
+
+
+// Gestion de la caisse
+$lang['case_account']                   = 'Caisse / Solde';
+$lang['case_manage']                    = 'Gestion de la Caisse / Banque';
+$lang['case_list']                      = 'Liste de la Caisse / Banque';
+$lang['case']                           = 'Caisse';
+$lang['case_history']                   = 'Transactions de la Caisse';
+$lang['balance']                        = 'Solde';
+$lang['balance_due']                    = 'Solde';
+$lang['statement_heading_date']         = 'Date';
+$lang['statement_heading_details']      = 'Détails de la Transaction';
+$lang['statement_heading_amount']       = 'Montant';
+$lang['statement_heading_payments']     = 'Paiements';
+$lang['statement_heading_balance']      = 'Solde';
+$lang['statement_invoice_details']      = 'Facture %s - Date d\'échéance %s';
+$lang['statement_payment_details']      = 'Paiement %s, Facture %s';
+
+// Transfert
+$lang['account_balances_transfer']      = 'Transfert';
+$lang['inter_cash_transfer']            = 'Transfert Inter-caisses';
+$lang['new_transfer_record']            = 'Nouvel Enregistrement de Transfert';
+$lang['transfer_heading_id']            = 'ID';
+$lang['transfer_heading_welding_case']  = 'Caisse Source';
+$lang['transfer_heading_target_case']   = 'Caisse Cible';
+$lang['transfer_heading_source_amount'] = 'Montant Envoyé';
+$lang['transfer_heading_target_amount'] = 'Montant Reçu';
+$lang['transfer_heading_description']   = 'Description';
+$lang['transfer_heading_date']          = 'Date du Transfert';
+$lang['transfer_heading_process_date']  = 'Date de Traitement';
+$lang['transfer_heading_process_staff'] = 'Personnel de Traitement';
+$lang['transfer_warning_msg']           = 'Vous pouvez essayer de transférer entre différentes caisses :)';
+$lang['transfer_success_msg']           = 'Le transfert a été effectué avec succès';
+$lang['transfer_not_found']             = 'Enregistrement de transfert introuvable';
+$lang['transfer_edit_heading']          = 'Modifier l\'Enregistrement de Transfert';
+$lang['transfer_add_heading']           = 'Ajouter un Enregistrement de Transfert';
+
+// Solde Initial du Compte
+$lang['account_balances_deliver']       = 'Solde Initial du Compte';
+$lang['balance_heading_account']        = 'Caisse';
+$lang['account_balances_id']            = 'ID';
+$lang['balance_heading_open_balance']   = 'Solde d\'Ouverture';
+$lang['balance_heading_currency']       = 'Devise';
+$lang['balance_not_found']              = 'Caisse non trouvée !';
+$lang['balance_success_msg']            = 'Le solde initial du compte a été mis à jour avec succès';
+$lang['cash_opening_deliver_balance']   = 'solde d\'Ouverture / Reporté de Caisse';
+$lang['money_transfer_to_cash']         = 'transfert d\'argent vers la caisse';
