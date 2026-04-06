f2a6f76ab9135770c55a0a021defaa18f3d5e906
HRM King David II <uscapitalfundingii@gmail.com>
2026-04-06 10:48:41 +0400

    Create account_balances_lang.php

---
+++ crm/modules/account_balances/language/german/account_balances_lang.php
@@ -0,0 +1,74 @@
+<?php
+
+// Version 105
+$lang['account_balance_enable_multi_currency_for_invoice'] = 'Aktivieren Sie mehrere Währungen für Rechnungen';
+$lang['account_balance_enable_multi_currency_for_estimate'] = 'Aktivieren Sie mehrere Währungen für Kostenvoranschläge';
+
+
+
+
+// Version 102
+
+$lang['account_week_balance_title']     = 'Wöchentliches Bilanzdiagramm';
+$lang['account_entry']                  = 'Kontoeinkommen';
+$lang['account_income_new']             = 'Neues Einkommen';
+$lang['income_success_msg']             = 'Einkommensdatensatz erfolgreich verarbeitet!';
+$lang['income_not_found']               = 'Einkommensdatensatz nicht gefunden!';
+$lang['transfer_of_money_from_cash']    = 'Transfer money in';
+
+
+$lang['account_balance_money_in']       = 'Geld rein';
+$lang['account_balance_money_out']      = 'Geld aus';
+$lang['account_balance_carryover']      = 'Übertragssaldo';
+
+// Version. 101
+$lang['account_withdrawal']             = 'Kontoabhebung';
+$lang['account_withdrawal_new']         = 'Neue Auszahlung';
+$lang['money_withdraw']                 = 'Zurückziehen';
+
+
+// Kassenverwaltung
+$lang['case_account']                   = 'Kasse / Saldo';
+$lang['case_manage']                    = 'Kassen- / Bankverwaltung';
+$lang['case_list']                      = 'Kassen- / Bankliste';
+$lang['case']                           = 'Kasse';
+$lang['case_history']                   = 'Kassatransaktionen';
+$lang['balance']                        = 'Saldo';
+$lang['balance_due']                    = 'Saldo';
+$lang['statement_heading_date']         = 'Datum';
+$lang['statement_heading_details']      = 'Transaktionsdetails';
+$lang['statement_heading_amount']       = 'Betrag';
+$lang['statement_heading_payments']     = 'Zahlungen';
+$lang['statement_heading_balance']      = 'Saldo';
+$lang['statement_invoice_details']      = 'Rechnung %s - Fälligkeitsdatum %s';
+$lang['statement_payment_details']      = 'Zahlung %s, Rechnung %s';
+
+// Überweisung
+$lang['account_balances_transfer']      = 'Überweisung';
+$lang['inter_cash_transfer']            = 'Interne Kassenüberweisung';
+$lang['new_transfer_record']            = 'Neuer Überweisungseintrag';
+$lang['transfer_heading_id']            = 'ID';
+$lang['transfer_heading_welding_case']  = 'Quellkasse';
+$lang['transfer_heading_target_case']   = 'Zielkasse';
+$lang['transfer_heading_source_amount'] = 'Überwiesener Betrag';
+$lang['transfer_heading_target_amount'] = 'Erhaltener Betrag';
+$lang['transfer_heading_description']   = 'Beschreibung';
+$lang['transfer_heading_date']          = 'Überweisungsdatum';
+$lang['transfer_heading_process_date']  = 'Abwicklungsdatum';
+$lang['transfer_heading_process_staff'] = 'Bearbeitet von';
+$lang['transfer_warning_msg']           = 'Sie können versuchen, zwischen verschiedenen Kassen zu überweisen :)';
+$lang['transfer_success_msg']           = 'Die Überweisung wurde erfolgreich durchgeführt';
+$lang['transfer_not_found']             = 'Überweisungseintrag nicht gefunden';
+$lang['transfer_edit_heading']          = 'Überweisungseintrag bearbeiten';
+$lang['transfer_add_heading']           = 'Überweisungseintrag hinzufügen';
+
+// Kontoanfangssaldo
+$lang['account_balances_deliver']       = 'Kontoanfangssaldo';
+$lang['balance_heading_account']        = 'Kasse';
+$lang['account_balances_id']            = 'ID';
+$lang['balance_heading_open_balance']   = 'Eröffnungssaldo';
+$lang['balance_heading_currency']       = 'Währung';
+$lang['balance_not_found']              = 'Kasse nicht gefunden!';
+$lang['balance_success_msg']            = 'Der Kontoanfangssaldo wurde erfolgreich aktualisiert';
+$lang['cash_opening_deliver_balance']   = 'Kassenanfang / Übertragungsbilanz';
+$lang['money_transfer_to_cash']         = 'geldüberweisung zur kasse';
